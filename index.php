04b982251a7a3031c20c3dd3c3028fd862a903b6
emilads <40250399+emilads@users.noreply.github.com>
2018-06-14 02:29:35 +0400

    init commit

---
+++ index.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+    
+	<title>ter</title>
+</head>
+<body>
+<p>terter</p>
+</body>
+</html>
